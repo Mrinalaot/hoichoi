cc04b9e6621d10a6d46fe05f2a34fe9f4b819041
tzsk <mailtokmahmed@gmail.com>
2018-07-15 22:17:08 +0530

    Initial Commit

---
+++ Hoichoi.user.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @name         Hoichoi
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Hoichoi TV Downloader
+// @author       tzsk
+// @match        https://www.hoichoi.tv/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-1.12.4.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var src ='';
+    var button = false;
+    var interval;
+    var fileName ='';
+
+    $(document).on('click', '.video-tray-item', function () {
+        if (interval) {
+            clearTimeout(interval);
+        }
+        interval = setInterval(check, 2000);
+    });
+
+    $(document).on('click', '.vjs-resolution-menu li', function () {
+        if (interval) {
+            clearTimeout(interval);
+        }
+        interval = setInterval(check, 2000);
+    });
+
+    function check(){
+        var videoLink = document.querySelector('.vjs-tech');
+        if(videoLink){
+            fileName = document.querySelector(".episode-name .name").innerText;
+            console.log('Ready to Hoichoi...');
+
+            clearTimeout(interval);
+            src = videoLink.src;
+            showButton();
+        }
+    }
+
+    function showButton() {
+        if (button) {
+            $('.tzsk_download').remove();
+            button = false;
+        }
+        if (! button) {
+            var quality = $('.current-resolution').text();
+            var style = 'font-weight:bold;';
+            var node = '<a href="#" download="'+fileName+
+                '.mp4" class="tzsk-download site-color" style="'+style+
+                '">Download in '+quality+'</a>';
+            $('.overlay-episode-details').append(node);
+        }
+        $('.tzsk_download').attr('href', src);
+        button = true;
+        if (interval) {
+            clearTimeout(interval);
+        }
+    }
+
+    $(document).on('click', '.tzsk-download', function(e) {
+        e.preventDefault();
+        alert("Right Click on this button then click 'Save link as' to download.");
+        return false;
+    });
+})();
---
+++ README.md
@@ -0,0 +1,19 @@
+# Hoichoi TV Downloader
+
+This script requires Tampermonkey extension to be present in Chrome
+
+## Installation
+
+1. Install `Tampermonkey` extension from Chrome Webstore.
+2. Click on `Tampermonkey` icon and enble it if it is not already enabled.
+3. Click on Dasboard from the `Tampermonkey` menu.
+4. Click on the `+` icon from the tabs.
+5. Replace the entire code with the content of the above `Hoichoi.user.js` file.
+6. Then press `Ctrl+S` to save the script.
+7. Enable the script by clicking the knob on `Installed Scripts` list page.
+
+> NOTE: Make sure to enable this script from the `Installed Scripts` list and also enable `Tampermonkey`. Otherwise, it won't work.
+
+## Usage
+
+Open any video on `hoichoi.tv` and a Download link will appear right beside the Video Information. You can change the Video Quality from the Video Controls and it will adjust accordingly.
